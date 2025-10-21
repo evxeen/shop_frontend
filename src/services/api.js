49e917e414fa06aa59d8d864dabe703856338999
evxeen <evxeen@gmail.com>
2025-10-21 15:14:12 +0300

    con at rail

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,12 @@
-// src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
     timeout: 10000,
 });
 
-// Интерцептор для обработки ошибок
 api.interceptors.response.use(
     (response) => response,
     (error) => {
